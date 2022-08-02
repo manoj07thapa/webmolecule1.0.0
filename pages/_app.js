930c2fae564e0bde8b84aba6d9a64d313685a022
Manoj Thapa <mnjinvincible@gmail.com>
2022-08-02 13:31:25 +0545

    removed nav bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import "../styles/globals.css";
 import { Fragment } from "react";
-import Navbar from "../components/header/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
-      <Navbar />
       <Component {...pageProps} />
     </Fragment>
   );
