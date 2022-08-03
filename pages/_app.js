20e8866655a5ed4d7c199ae010c0ddb4ee34646b
Manoj Thapa <mnjinvincible@gmail.com>
2022-08-03 14:30:17 +0545

    dynamic route added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { Fragment } from "react";
-import Navbar from "../Navbar";
+import Navbar from "../components/header/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
