2a2a6a8d1e80753cd1758456d4d8b4ef4d452c2c
Manoj Thapa <mnjinvincible@gmail.com>
2022-08-03 15:14:16 +0545

    whatever fuck it

--- pages/courses/[param].js
+++ pages/courses/[param].js
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 
-const Course = () => {
+export default function () {
   const { query } = useRouter();
   return <div>{query.param}</div>;
-};
-export default Course;
+}
