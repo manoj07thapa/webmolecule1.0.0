6623c3e3dc7fe846d411ec7ab3a8fda9bf86ded9
Manoj Thapa <mnjinvincible@gmail.com>
2022-08-03 16:24:03 +0545

    fuck u amplify

--- components/header/Navbar.js
+++ components/header/Navbar.js
@@ -1,16 +1,130 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import {
+  MenuIcon,
+  XIcon,
+  BookmarkAltIcon,
+  CalendarIcon,
+  ChartBarIcon,
+  CursorClickIcon,
+  RefreshIcon,
+  ShieldCheckIcon,
+  SupportIcon,
+  ViewGridIcon,
+} from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import MyLink from "./MyLink";
-import { courses, solutions } from "./navData";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
+const MyLink = React.forwardRef((props, ref) => {
+  let { href, children, ...rest } = props;
+  return (
+    <Link href={href}>
+      <a
+        ref={ref}
+        {...rest}
+        className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-100"
+      >
+        {children}
+      </a>
+    </Link>
+  );
+});
+
+export const courses = [
+  {
+    name: "All courses",
+    description:
+      "Get all of your questions answered in our forums or contact support.",
+    icon: ViewGridIcon,
+    key: "all",
+  },
+  {
+    name: "Frontend",
+    description:
+      "Get all of your questions answered in our forums or contact support.",
+    icon: SupportIcon,
+    key: "frontend",
+  },
+  {
+    name: "Backend",
+    description:
+      "Learn how to maximize our platform to get the most out of it.",
+    icon: BookmarkAltIcon,
+    key: "backend",
+  },
+  {
+    name: "Web Design",
+    description:
+      "See what meet-ups and other events we might be planning near you.",
+    icon: CalendarIcon,
+    key: "webDesign",
+  },
+  {
+    name: "Full Stack ",
+    description: "Understand how we take your privacy seriously.",
+    icon: ShieldCheckIcon,
+    key: "fullstack",
+  },
+  {
+    name: "Database ",
+    description: "Understand how we take your privacy seriously.",
+    icon: ShieldCheckIcon,
+    key: "database",
+  },
+  {
+    name: "AWS Amplify",
+    description: "Understand how we take your privacy seriously.",
+    icon: ShieldCheckIcon,
+    key: "awsAmplify",
+  },
+];
+
+export const solutions = [
+  {
+    name: "View all solutions",
+    description:
+      "Get a better understanding of where your traffic is coming from.",
+    icon: ViewGridIcon,
+    key: "all",
+  },
+  {
+    name: "Portfolio",
+    description:
+      "Get a better understanding of where your traffic is coming from.",
+    icon: ChartBarIcon,
+    key: "portfolio",
+  },
+  {
+    name: "E-commerce Store",
+    description: "Speak directly to your customers in a more meaningful way.",
+    icon: CursorClickIcon,
+    key: "ecommerce",
+  },
+  {
+    name: "Student management system",
+    description: "Your customers' data will be safe and secure.",
+    icon: ShieldCheckIcon,
+    key: "sms",
+  },
+  {
+    name: "Account management system",
+    description: "Connect with third-party tools that you're already using.",
+    icon: ViewGridIcon,
+    key: "ams",
+  },
+  {
+    name: "Human resources management system",
+    description:
+      "Build strategic funnels that will drive your customers to convert",
+    icon: RefreshIcon,
+    key: "hrms",
+  },
+];
 
 export default function Navbar({}) {
   const router = useRouter();
