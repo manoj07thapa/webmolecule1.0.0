9ef6bc1c84fb98637fdf377395b68853fd67c764
Manoj Thapa <mnjinvincible@gmail.com>
2022-08-04 13:37:59 +0545

    added popover with routing

--- components/header/CoursePopoverMenu.js
+++ components/header/CoursePopoverMenu.js
@@ -1,28 +1,30 @@
 import { Popover, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 const courses = [
   {
-    name: "Insights",
+    name: "Frontend",
     description: "Measure actions your users take",
-    href: "##",
+    href: "frontend",
     icon: IconOne,
   },
   {
-    name: "Automations",
+    name: "Backend",
     description: "Create your own targeted content",
-    href: "##",
+    href: "backend",
     icon: IconTwo,
   },
   {
-    name: "Reports",
+    name: "Web Design",
     description: "Keep track of your growth",
-    href: "##",
+    href: "webDesign",
     icon: IconThree,
   },
 ];
 
 const CoursePopoverMenu = () => {
+  const router = useRouter();
   return (
     <Popover className="relative">
       {({ open }) => (
@@ -32,7 +34,7 @@ const CoursePopoverMenu = () => {
             ${open ? "" : "text-opacity-90"}
             group inline-flex items-center rounded-md bg-orange-700 px-3 py-2 text-base font-medium text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
           >
-            <span>Solutions</span>
+            <span>Courses</span>
             <ChevronDownIcon
               className={`${open ? "" : "text-opacity-70"}
               ml-2 h-5 w-5 text-orange-300 transition duration-150 ease-in-out group-hover:text-opacity-80`}
@@ -49,44 +51,37 @@ const CoursePopoverMenu = () => {
             leaveTo="opacity-0 translate-y-1"
           >
             <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
-              <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
-                  {courses.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
-                    >
-                      <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
-                        <item.icon aria-hidden="true" />
+              {({ close }) => (
+                <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                  <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
+                    {courses.map((item) => (
+                      <div
+                        key={item.name}
+                        className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                      >
+                        <button
+                          className="flex items-center"
+                          onClick={() => {
+                            router.push(`/courses/${item.href}`), close();
+                          }}
+                        >
+                          <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
+                            <item.icon aria-hidden="true" />
+                          </div>
+                          <div className="ml-4 flex flex-col items-start">
+                            <p className="text-sm font-medium text-gray-900">
+                              {item.name}
+                            </p>
+                            <p className="text-sm text-gray-500">
+                              {item.description}
+                            </p>
+                          </div>
+                        </button>
                       </div>
-                      <div className="ml-4">
-                        <p className="text-sm font-medium text-gray-900">
-                          {item.name}
-                        </p>
-                        <p className="text-sm text-gray-500">
-                          {item.description}
-                        </p>
-                      </div>
-                    </a>
-                  ))}
+                    ))}
+                  </div>
                 </div>
-                <div className="bg-gray-50 p-4">
-                  <a
-                    href="##"
-                    className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
-                  >
-                    <span className="flex items-center">
-                      <span className="text-sm font-medium text-gray-900">
-                        Documentation
-                      </span>
-                    </span>
-                    <span className="block text-sm text-gray-500">
-                      Start integrating products and tools
-                    </span>
-                  </a>
-                </div>
-              </div>
+              )}
             </Popover.Panel>
           </Transition>
         </>
